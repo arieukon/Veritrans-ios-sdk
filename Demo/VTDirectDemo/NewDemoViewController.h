5cc8a4feeb923ad32158646a032d1f727a85ed92
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-02-23 06:02:29 +0700

    create simple class for new corekit implementation

---
+++ Demo/VTDirectDemo/NewDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewDemoViewController.h
+//  VTDirectDemo
+//
+//  Created by Nanang Rafsanjani on 2/22/17.
+//  Copyright © 2017 Midtrans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewDemoViewController : UIViewController
+
+@end
